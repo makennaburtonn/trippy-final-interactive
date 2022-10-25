25cf7ec0a3ef577253347ac6edbafcc5860b674f
makennaburtonn <116090459+makennaburtonn@users.noreply.github.com>
2022-10-25 11:20:07 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  a=height/2
+
+  colorMode(HSB, 360, 100, 100);
+  rectMode(CENTER);
+  noStroke();
+  
+}
+
+function draw() {   
+  background(mouseY / 2, 100, 100);
+
+  fill(360 - mouseY / 2, 100, 100);
+  strokeWeight(2);
+  stroke(0);
+  rect(windowWidth/2, windowHeight/2, mouseX + 1, mouseX + 1);
+ 
+  fill(300 - mouseY / 2, 100, 100);
+  strokeWeight(3);
+  stroke(0);
+  text('TRIPPY', windowWidth/2,windowHeight/2);
+  textFont('courier');
+  textAlign(CENTER,CENTER);
+  textSize(80 + (mouseX / width)*200);
+  
+}
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
